6e5aa0ad477818ac669389e702b9fd320d574540
Zan Smirnov <c15437072@mydit.ie>
2018-10-15 18:07:55 +0100

    Added crop and improvements

--- ocr.py
+++ ocr.py
@@ -8,12 +8,42 @@ cv2.imshow('Original gray', gray_img)
 
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 cl1 = clahe.apply(gray_img)
-cv2.imshow('Clahe equalized gray', cl1)
 
-cl1 = cv2.bilateralFilter(cl1, 9, 75, 75)
-thresholded_img = cv2.adaptiveThreshold(cl1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 2)
+cl2 = cv2.bilateralFilter(cl1, 9, 75, 75)
+thresholded_img = cv2.adaptiveThreshold(cl2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
 #ret, thresholded_img = cv2.threshold(cl1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+top_pixel = float('inf')
+bottom_pixel = 0
+right_pixel = 0
+left_pixel = float('inf')
+
+for y, r in enumerate(thresholded_img):
+    for x, c in enumerate(r):
+        if thresholded_img[y, x] == 0:
+            if y < top_pixel:
+                top_pixel = y
+            if y > bottom_pixel:
+                bottom_pixel = y
+            if x > right_pixel:
+                right_pixel = x
+            if x < left_pixel:
+                left_pixel = x
+
+cv2.rectangle(thresholded_img,(left_pixel, top_pixel),(right_pixel,bottom_pixel),(0,255,0),3)
 cv2.imshow('Clahe processed img', thresholded_img)
+
+cl1 = cl1[top_pixel-5:top_pixel+(bottom_pixel-top_pixel)+5, left_pixel-5:left_pixel+(right_pixel-left_pixel)+5]
+edges = cv2.Canny(cl1,100,200)
+cv2.imshow('Edges',edges)
+# https://stackoverflow.com/questions/44047819/increase-image-brightness-without-overflow/44054699#44054699
+dilated_img = cv2.dilate(cl1, np.ones((7,7), np.uint8)) 
+bg_img = cv2.medianBlur(dilated_img, 21)
+diff_img = 255 - cv2.absdiff(cl1, bg_img)
+norm_img = diff_img.copy() # Needed for 3.x compatibility
+cv2.normalize(diff_img, norm_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+cv2.imshow('test', norm_img)
+cv2.imshow('Clahe equalized gray', cl1)
 
 
 histogram_equalized = cv2.equalizeHist(gray_img.ravel())
