6fc766e535ae6ae7935084b5b749eb501459ca32
Zan Smirnov <C15437072@mydit.ie>
2018-10-18 11:24:30 +0100

    Added comments

--- ocr.py
+++ ocr.py
@@ -2,10 +2,45 @@
 # This script takes in an image of text, for example a poor quality Russian apartment sale contract
 # and tries to improve the quality of the image as to make the text more readable.
 
-# The method I use is dilating and blurring the image and getting the difference between
+# The general method I use is dilating and blurring the image and getting the difference between
 # the blurred image and the dilated image to make the text stand out and cancel out the
 # background. Just using thresholding such as Clahe works well too but these two methods
 # in combination provide the best result. Clahe fixes the lighting and the subtraction removes further lighting issues and noise.
+
+# To crop the image we blur it, threshold it and get the topmost, bottommost, rightmost, and leftmost black
+# text pixels to use as the cropping rectangle.
+
+# First we convert the image to grayscale.
+# I chose to convert the image to grayscale instead of using the L channel of the LAB color space 
+# because the results were the same but using grayscale is easier.
+
+# Then we equalize the histogram to minimize the lighting issues.
+# Clahe here is preferred over normal equalization because it it adaptive.
+# This means that instead of equalizing the global histogram, clahe equalizes local areas 
+# that are the size of the kernel which in this case is 8,8.
+# Adaptive thresholding works well in our test image because the lighting is not even and
+# changes a lot from area to area of the image.
+
+# After CLAHE I apply smoothing bilateral filter in order to get rid of some of the noisy black spots that isn't text.
+# The bilateral filter basically dissolves the black noise in the background into the background.
+# This denoised blurred image is then thresholded and used to acquire the topmost, bottommost, rightmost, and leftmost black pixels for cropping.
+
+# After cropping the image, we then apply our dilation, blurring and difference between the original clahe'd image and the dilated image.
+# When we dilate and blur we're left with a blurry image that has the background of the image without the text.
+# The text dissolves into the background basically.
+# Now we get the difference between the original clahe'd image and the blurred image of the background.
+# This means that the difference between the text pixels will be great because in the original image
+# the text is very black but in the blurred image it is blurred with the background and takes the color of the background
+# which isn't as black.
+# The difference between the background pixels however will not be as great because they both will be about the same value.
+# This means that the text will stand out while the background will be subtracted.
+
+# After we got the image of the difference between the background blurred image and the original image,
+# we normalize the image to increase the contrast and make the text stand out even more.
+# Then we display the image.
+
+# I use the method for dilating, blurring and canceling out the background in the SO answer below.
+# Masek, D. (2017). Increase image brightness without overflow. [online] stackoverflow.com. Available at: https://stackoverflow.com/questions/44047819/increase-image-brightness-without-overflow/44054699#44054699 [Accessed 18 Oct. 2018].
 
 import cv2
 import numpy as np
@@ -18,22 +53,11 @@ f = easygui.fileopenbox()
 img = cv2.imread(f)
 
 # Convert the image to gray
-# I chose to convert it to grayscale instead of using the L channel of the LAB color space 
-# because the results were the same but using grayscale is easier.
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Create a clahe object so that we can apply histogram equalization.
-# Clahe here is preferred over normal equalization because it it adaptive.
-# This means that instead of equalizing the global histogram, clahe equalizes local areas 
-# that are the size of the kernel which in this case is 8,8.
-# Adaptive thresholding works well in our test image because the lighting is not even and
-# changes a lot from area to area of the image.
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 # Apply the clahe equalization
 cl1 = clahe.apply(gray_img)
-
-# Now we apply a smoothing bilateral filter in order to get rid of some of the noisy black spots that isn't text.
-# The bilateral filter basically dissolves the black noise in the background into the background.
 cl2 = cv2.bilateralFilter(cl1, 9, 75, 75)
 # Now we threshold the image, again using adaptive thresholding in order to make all the text black
 # and make all of the background white. While we did apply clahe, there are still small differences in lighting
@@ -70,22 +94,12 @@ cv2.rectangle(thresholded_img,(left_pixel, top_pixel),(right_pixel,bottom_pixel)
 
 # Now we crop the original clahe'd image.
 cl1 = cl1[top_pixel-5:top_pixel+(bottom_pixel-top_pixel)+5, left_pixel-5:left_pixel+(right_pixel-left_pixel)+5]
-
-# I got the method for dilating, blurring and canceling out the background in the SO answer below.
-# https://stackoverflow.com/questions/44047819/increase-image-brightness-without-overflow/44054699#44054699
 # Dilate the image to blur everything and mostly get rid of the text
 dilated_img = cv2.dilate(cl1, np.ones((7,7), np.uint8)) 
 # Median blur the image to further get rid of the text.
 background_blurred_img = cv2.medianBlur(dilated_img, 21)
-# At this point we're left with a blurry image that has the background of the image without the text.
-# Now we get the difference between the original clahe'd image and the blurred image of the background.
-# This means that the difference between the text pixels will be great because in the original image
-# the text is very black but in the blurred image it is blurred with the background and takes the color of the background
-# which isn't as black.
-# The difference between the background pixels however will not be as great because they both will be about the same value.
 diff_img = 255 - cv2.absdiff(cl1, background_blurred_img)
 normalized_img = diff_img.copy()
-# Now we normalize the image to increase the contrast and make the text stand out more
 cv2.normalize(diff_img, normalized_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
 
 # Finally show the image
